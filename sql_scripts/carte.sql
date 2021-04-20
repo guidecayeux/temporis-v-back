c6d06cef6871b8276f480279d0c2b003ae294642
Guillaume <gui.decayeux@gmail.com>
2021-04-20 22:37:01 +0200

    ajouts des cartes

--- sql_scripts/carte.sql
+++ sql_scripts/carte.sql
@@ -1,12 +1,17 @@
+-- Table: public.carte
+
+DROP TABLE public.carte;
 
 CREATE TABLE public.carte
 (
     id integer NOT NULL GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 1 MINVALUE 1 MAXVALUE 2147483647 CACHE 1 ),
     name text COLLATE pg_catalog."default" NOT NULL,
+    color text COLLATE pg_catalog."default",
+    super_card boolean,
     CONSTRAINT carte_pkey PRIMARY KEY (id)
 )
 
-TABLESPACE pg_default;
+    TABLESPACE pg_default;
 
 ALTER TABLE public.carte
-    OWNER to root;
+    OWNER to postgres;
